a886f5134e8191594612a9bb0de7c089ad5a7799
Fawzi Abo Shkara <fawzi.aboshkara@gmail.com>
2022-02-07 00:02:20 +0200

    Add Participant dataclass. Add file and misc utils (#2)
    * Add the `Participant` dataclass.
    * Add `FileUtils` and `MiscUtils` in `secret_santa/util` package to load the players from a file later and check the players' permutation.

---
+++ secret_santa/model/participant.py
@@ -0,0 +1,37 @@
+from attr import dataclass
+
+
+@dataclass(eq=False, frozen=True)
+class Participant:
+    """
+    A data class which holds the Secret Santa participants' information.
+
+    Attributes:
+        full_name: The participant's full name.
+        phone_number: The participant's phone number.
+        nickname: The participant's nickname which will be used.
+
+    """
+
+    full_name: str
+    phone_number: str
+    nickname: str | None = None
+
+    def __eq__(self, other) -> bool:
+        """
+        Custom equals method for this data class to check if two participants are identical.
+
+        Args:
+            other: The other participant to check against self
+
+        Returns:
+            True if all the properties of ``self`` and ``other`` match, False otherwise.
+
+        """
+        if not isinstance(other, Participant):
+            return NotImplemented
+        return (
+            self.full_name == other.full_name
+            and self.phone_number == other.phone_number
+            and self.nickname == other.nickname
+        )
---
+++ secret_santa/util/file.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+from typing import List
+
+
+class FileUtils:
+    """A class aggregating all the file utilities needed (provided as static methods)."""
+
+    @staticmethod
+    def create_file(full_file_name: str | Path, content: str) -> None:
+        """
+        Create a new file containing the ``content`` at the path specified ``full_file_name``.
+
+        Args:
+            full_file_name: Path (relative / absolute) to the file to be created.
+            content: The content to write to the file.
+
+        """
+        with open(full_file_name, "w", encoding="utf8") as new_file:
+            new_file.write(content)
+
+    @staticmethod
+    def read_file(file_name: str, read_line_by_line: bool = False) -> str | List[str]:
+        """
+        Read the contents of the file at ``file_name``.
+
+        Args:
+            file_name:
+            read_line_by_line:
+
+        Returns:
+            The contents of the file in one string in case ``read_line_by_line`` is False /
+                as a list of strings read line by line in case ``read_line_by_line`` is True.
+        """
+        with open(file_name) as f:
+            content = f.readlines() if read_line_by_line else f.read()
+        return content
