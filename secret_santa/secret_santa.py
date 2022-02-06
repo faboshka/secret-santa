e381f965959e1ed4f417b25c84bde7cfc6e8e65f
FawziAS <fawzi.aboshkara@gmail.com>
2022-02-06 22:13:38 +0200

    Initial project setup

---
+++ secret_santa/secret_santa.py
@@ -0,0 +1,14 @@
+class SecretSanta:
+    def __init__(self) -> None:
+        pass
+
+    def run(self) -> int:
+        pass
+
+
+def main() -> int:
+    return SecretSanta().run()
+
+
+if __name__ == "__main__":
+    main()
