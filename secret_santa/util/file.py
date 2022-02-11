65a2984965cd156a87844ba578e5bb49653bc1c1
FawziAS <fawzi.aboshkara@gmail.com>
2022-02-11 04:02:59 +0200

    Edit methods documentation in secret_santa/util/file.py and secret_santa/util/participant.py

--- secret_santa/util/file.py
+++ secret_santa/util/file.py
@@ -24,12 +24,14 @@ class FileUtils:
         Read the contents of the file at ``file_name``.
 
         Args:
-            file_name:
-            read_line_by_line:
+            file_name: Path (relative / absolute) to the file to be read.
+            read_line_by_line: If True, the file's contents will be read line by line and a list will be returned,
+                otherwise the file's contents will be read as one string to be returned to the user.
 
         Returns:
             The contents of the file in one string in case ``read_line_by_line`` is False /
                 as a list of strings read line by line in case ``read_line_by_line`` is True.
+
         """
         with open(file_name) as f:
             content = f.readlines() if read_line_by_line else f.read()
